7478300316d5606068087eedd354a504b112c699
yutadd <spyk4m49@gmail.com>
2022-07-11 18:25:32 +0900

    make easy more readable stats & Fix Transaction issues

--- src/main/java/discord/yutarpg/work/App.java
+++ src/main/java/discord/yutarpg/work/App.java
@@ -3,6 +3,7 @@ package discord.yutarpg.work;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -12,7 +13,9 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -23,7 +26,6 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import priv.key.Bouncycastle_Secp256k1;
@@ -38,11 +40,13 @@ public class App extends ListenerAdapter {
 	static JDA jda;
 	static Socket node;
 	static HashMap<String,BigInteger>id_priv=new HashMap<String,BigInteger>();
-	static HashMap<String, String> id_pub = new HashMap<>();
+	static HashMap<String, String> id_pub = new HashMap<String,String>();
 	static int day = LocalDateTime.now().getDayOfMonth();
-	static HashMap<String, BigDecimal> role_income = new HashMap<>();
+	static HashMap<String, BigDecimal> role_income = new HashMap<String,BigDecimal>();
 	static BigDecimal netwall = new BigDecimal(10000);
 	static HashMap<String,BigDecimal> utxo=new HashMap<String,BigDecimal>();
+	static HashMap<String,Entry<MessageChannel,String>> transactionHashes=new HashMap<String,Entry<MessageChannel,String>>();
+	static long cacheTimeStampMilli=5000;
 	/**
 	 * 0:確認中
 	 * １:ok
@@ -90,7 +94,7 @@ public class App extends ListenerAdapter {
 			e.printStackTrace();
 		}
 		new Diary().start();
-		
+
 	}
 
 	String src_sign = null;
@@ -101,110 +105,160 @@ public class App extends ListenerAdapter {
 		File f = new File("wallet.txt");
 		MessageChannel channel = event.getChannel();
 		Message msg = event.getMessage();
-		if (msg.getContentRaw().startsWith("%balance")) {
+		if (msg.getContentRaw().startsWith("%create")) {
+			BufferedReader fr=null;
 			try {
-				node.getOutputStream().write("balances~\r\n".getBytes());
-				Thread.sleep(100);
-			} catch (Exception e1) {
+				fr = new BufferedReader(new FileReader(f));
+				FileWriter fw=new FileWriter(f,true);
+				String s=null;
+				boolean isWrited=false;
+				while((s=fr.readLine())!=null) {
+					if(s.split("~")[0].equals(msg.getAuthor().getId())) {
+						isWrited=true;
+					}
+				}
+				System.out.println("???0");
+				if(!isWrited) {
+					Secp256k1 g = new Secp256k1();
+					BigInteger[] pubK1 = new BigInteger[2];
+					Random rn = new Random();
+					BigInteger   priK = new BigInteger(255,rn);
+					pubK1 = g.multiply_G(priK);
+					byte[] priv=priK.toByteArray();
+					fw.append(msg.getAuthor().getId()+"~"+new BigInteger(priv).toString(16)+"\r\n");
+					loadWallets();
+					channel.sendMessage("さいふをつくったよ！").queue();
+				}else {
+					channel.sendMessage("もう作ってあるよ！"+id_pub.get(msg.getAuthor().getId())).queue();
+				}
+			} catch (Exception e) {
 				// TODO 自動生成された catch ブロック
-				e1.printStackTrace();
+				System.out.println("???");
+				e.printStackTrace();
+			}
+		}else if (msg.getContentRaw().startsWith("%balance")) {
+			if(System.currentTimeMillis()-cacheTimeStampMilli>=5000) {
+				channel.sendMessage("残額の キャッシュを更新するね").queue();
+				try {
+					node.getOutputStream().write("balances~\r\n".getBytes());
+					Thread.sleep(900);
+				} catch (Exception e1) {
+					// TODO 自動生成された catch ブロック
+					e1.printStackTrace();
+				}
 			}
 			try {
-				channel.sendMessage(utxo.get(id_pub.get(msg.getAuthor().getId()).split("0x0a")[0]).toString()).queue();;
+				String pub=id_pub.get(msg.getAuthor().getId()).split("0x0a")[0];
+				BigDecimal balance=utxo.get(pub);
+				if(balance==null) {
+					channel.sendMessage("残額が見つかったよ！残額は、\r\n"+0+"GWCだよ！").queue();
+				}else {
+					channel.sendMessage("残額が見つかったよ！残額は、\r\n"+balance+"GWCだよ！").queue();
+				}
 			}catch(Exception e) {
-				channel.sendMessage("さいふがなかったよ\r\nマイニングができてないだけかも").queue();;
+				channel.sendMessage("さいふがなかったよ\r\nマイニングができてないだけかも\r\nさいふは%createコマンドでつくれるよ！").queue();
 			}
 		} else if (msg.getContentRaw().equals("%addr") || msg.getContentRaw().equals("%address")) {
 			channel.sendMessage("きみのさいふは" + id_pub.get(event.getAuthor().getId()) + "だよ！").queue();
 		} else if (msg.getContentRaw().equals("%help")) {
 			String s = "コマンド:gear:：\r\n" +
 					"├`%help : ヘルプ`\r\n" +
+					"├`%create : 財布を作るよ！`\r\n" +
 					"├`%balance : おかねの　のこりだよ！`\r\n" +
 					"│　└`%balance <ID> : ほかのフレンズ　の残高だよ！`\r\n" +
 					"├`%addr : きみの　アドレスだよ！`\r\n" +
 					"│　└`%address : うえとおんなじ！`\r\n" +
-					"├`%trade <取引相手(アドレスかメンションを使えるよ)> <amount> <fee> : とりひきを　できるよ！！`\r\n" +
-					"│　└`例 : %trade @Gosh#5490 40 0.5`\r\n" +
-					"表示に関して:eyes:：\r\n" +
-					"├エラーの　ことを　セルリアンって　いうよ！\r\n" +
-					"└ウォレットの　ことを　ボスって　よぶよ！\r\n" +
+					"├`%trade <取引相手(アドレスかメンションを使えるよ)> <amount> [fee] : とりひきを　できるよ！！`\r\n" +
+					"└　├`例 : %trade @Gosh#5490 40 0.5`\r\n" +
+					"　　└手数料は省略できるよ！*省略したら0.1が指定されたことになるよ*"+
 					"運営への報:moneybag:：\r\n" +
 					"└やくわりを　もってる　ひとには　１にちに１かい　コインが　ネットワークの　ウォレットから　しはらわれるよ！\r\n" +
 					"作者：狩りごっこが苦手なフレンズ";
 			channel.sendMessage(s).queue();
 		} else if (msg.getContentRaw().startsWith("%trade")) {
-			long time = System.currentTimeMillis();
-			String[] cmd = msg.getContentRaw().split(" ");
-
-			String S_1 = null;
-			System.out.println(cmd[1]);
-			String addr = null;
 			try {
-				if (App.jda.getUserByTag(msg.getContentStripped().split(" ")[1].split("@")[1]) == null) {
-					User m = App.jda.getUserByTag(msg.getContentStripped().split(" ")[1].split("@")[1]);
-					S_1 = m.getId();
-					addr = id_pub.get(S_1);
-				} else if(cmd[1].contains("!")) {
-					S_1 = cmd[1].split("<@!")[1].split(">")[0];
-					addr = id_pub.get(S_1);
-				}else if(cmd[1].contains("&")){
-					S_1 = cmd[1].split("<@&")[1].split(">")[0];
-					addr = id_pub.get(S_1);
-				}else if(id_pub.containsKey(cmd[1])) {
-					addr = id_pub.get(cmd[1]);
-				} else if(cmd[1].matches("[0-9][a-f]*0x0a[0-9][a-f]*")){
-					System.out.print("パターンにマッチした。");
-					addr = cmd[1];
-				}else {
-					channel.sendMessage("あてさき　が　わからないよ！").queue();;
-				}
-			} catch (Exception e) {
-				channel.sendMessage("あてさき　が　わからないよ！").queue();;
-			}
-			if (addr == null) {
-				channel.sendMessage("あてさき　が　わからないよ！").queue();;
-				return;
-			}
-			BigDecimal fee = new BigDecimal(0);
-			if (cmd[3] != null) {
+				long time = System.currentTimeMillis();
+				String[] cmd = msg.getContentRaw().split(" ");
+
+				String S_1 = null;
+				String addr = null;
 				try {
-					fee = new BigDecimal(cmd[3]);
+					if(cmd[1].contains("!")) {
+						S_1 = cmd[1].split("<@!")[1].split(">")[0];
+						addr = id_pub.get(S_1);
+					}else if(cmd[1].contains("&")){
+						S_1 = cmd[1].split("<@&")[1].split(">")[0];
+						addr = id_pub.get(S_1);
+					}else if(cmd[1].contains("<@")) {
+						S_1=cmd[1].split("<@")[1];
+						S_1=S_1.substring(0,S_1.length()-1);
+						System.out.println("parsed : "+S_1);
+						addr = id_pub.get(S_1);
+					}else if(id_pub.containsKey(cmd[1])) {
+						addr = id_pub.get(cmd[1]);
+					} else if(cmd[1].matches("[0-9a-fA-F]*0x0a[0-9a-fA-F]*")){
+						System.out.print("パターンにマッチした。");
+						addr = cmd[1];
+					}else {
+						channel.sendMessage("あてさき　が　わからないよ！").queue();
+						return;
+					}
+					if (addr == null) {
+						channel.sendMessage("あてさきのひと　が　さいふをもってないかも").queue();
+						return;
+					}
 				} catch (Exception e) {
-					channel.sendMessage("てすうりょうの　かきかたが　ふせいだよ！").queue();
+					channel.sendMessage("あてさきをしていしてね").queue();
 					return;
 				}
-			}
-			try {
-				//サインの原文作成：@なし
-				src_sign = id_pub.get(event.getAuthor().getId()) 
-						+addr.replace("0x0a", "0x0b") + "0x0c" + new BigDecimal(cmd[2])
-						+fee
-						+time;
-			} catch (Exception e) {
-				channel.sendMessage("きんがくの　かきかたが　ふせいだよ").queue();
-				return;
-			}
 
-			BigInteger[] sign = sign(event.getAuthor().getId(),
-					(hash(src_sign).getBytes()));
+				BigDecimal fee = new BigDecimal("0.1");
+				if (cmd.length<3) {
+					fee = new BigDecimal(cmd[3]);
+				}else {
+					channel.sendMessage("てすうりょうを　デフォルトの0.1使うよ！").queue();
+				}
+				try {
+					String address=id_pub.get(msg.getAuthor().getId());
+					BigDecimal balance=utxo.get(address.split("0x0a")[0]);
+					if(balance==null) {
+						channel.sendMessage("残額がないよ！").queue();
+						return;
+					}
+					if(cmd.length<2) {
+						channel.sendMessage("引数が少ないよ！").queue();
+						return;
+					}
+					if(balance.compareTo(fee.add(new BigDecimal(cmd[2])))<0) {
+						channel.sendMessage("お金が足りないよ！").queue();
+						return;
+					}
 
-			String transaction_sum = id_pub.get(event.getAuthor().getId()) +
-					"@" + addr.replaceFirst("0x0a", "0x0b") + "0x0c" + new BigDecimal(cmd[2]) +
-					"@" + fee +
-					"@" + time + "@" + sign[0].toString(16) + "0x0a" + sign[1].toString(16);
-			System.out.println(src_sign + "\r\n" + transaction_sum);
-			String kakunin = "`送信先→" + addr + "\r\n"
-					+ "数量→" + new BigDecimal(cmd[2]) + "`\r\n"
-					+"そうしんしたよ!";
-			channel.sendMessage(kakunin).queue();
-			try {
-				node.getOutputStream().write(("transaction~"+transaction_sum).getBytes());
-			} catch (IOException e) {
-				// TODO 自動生成された catch ブロック
-				e.printStackTrace();
-			}
+					//サインの原文作成：@なし
+					src_sign = id_pub.get(event.getAuthor().getId()) 
+							+addr.replace("0x0a", "0x0b") + "0x0c" + new BigDecimal(cmd[2])
+							+fee
+							+time;
+					BigInteger[] sign = sign(event.getAuthor().getId(),
+							(hash(src_sign).getBytes()));
+					String transaction_sum = id_pub.get(event.getAuthor().getId()) +
+							"@" + addr.replace("0x0a", "0x0b") + "0x0c" + new BigDecimal(cmd[2]) +
+							"@" + fee +
+							"@" + time + "@" + sign[0].toString(16) + "0x0a" + sign[1].toString(16);
+					System.out.println(src_sign + "\r\n" + transaction_sum);
+					String kakunin = "`送信先→" + addr + "\r\n"
+							+ "数量→" + new BigDecimal(cmd[2]) + "`\r\n"
+							+"そうしんしたよ!";
+					channel.sendMessage(kakunin).queue();
+					transactionHashes.put(sign[0].toString(16),new SimpleEntry<MessageChannel,String>(channel, msg.getAuthor().getId()));
+					node.getOutputStream().write(("transaction~"+transaction_sum+"\r\n").getBytes());
+				} catch (Exception e) {
+					channel.sendMessage("だめだった...エラーが発生したよ..\r\n%trade <取引相手(アドレスかメンションを使えるよ)> <amount> [fee]に従ってね").queue();
+					e.printStackTrace();
+				}
+			}catch(Exception e) {e.printStackTrace();}
 		} else if (msg.getContentRaw().startsWith("%check")) {
-			channel.sendMessage("node:"+((node.isClosed())?"ERROR":"OK!")).queue();;
+			channel.sendMessage("node:"+((node.isClosed())?"ERROR":"OK!")).queue();
 
 		}
 	}
--- src/main/java/discord/yutarpg/work/Receiver.java
+++ src/main/java/discord/yutarpg/work/Receiver.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Map.Entry;
+
+import net.dv8tion.jda.api.entities.MessageChannel;
 
 public class Receiver extends Thread{
 	@Override
@@ -13,24 +17,45 @@ public class Receiver extends Thread{
 		InputStream sock_in = null;
 		try {
 			sock_in =App.node.getInputStream();
+			InputStreamReader sock_is = new InputStreamReader(sock_in);
+			BufferedReader sock_br = new BufferedReader(sock_is);
+			String s=null;
+
+			while ((s=sock_br.readLine())!=null) {
+				try {
+					System.out.println(s);
+					if (s.startsWith("balance~")) {
+						String utxo=s.split("~")[1];
+						String[] arg=utxo.split(",");
+						App.utxo.put(arg[0], new BigDecimal(arg[1]));
+						System.out.println(arg[0]+","+arg[1]);
+						App.cacheTimeStampMilli=System.currentTimeMillis();
+					}else if(s.startsWith("ok~")) {
+						String sign=s.split("~")[1];
+						Entry<MessageChannel,String> entry=App.transactionHashes.get(sign);
+						entry.getKey().sendMessage("<@"+entry.getValue()+">さんの取引が認証されたよ！\r\nでも残額はマイニングされるまで増えないよ！").queue();
+					}else if(s.startsWith("reject~")) {
+						String sign=s.split("~")[1];
+						Entry<MessageChannel,String> entry=App.transactionHashes.get(sign);
+						entry.getKey().sendMessage("<@"+entry.getValue()+">さんの取引がなぜかネットワークのノードに拒否されたよ！").queue();
+						App.transactionHashes.remove(sign);
+					}else if(s.startsWith("block~")) {
+						ArrayList<String> contains=new ArrayList<String>();
+						for(Entry<String,Entry<MessageChannel,String>> entry:App.transactionHashes.entrySet()) {
+							if(s.contains(entry.getKey())) {
+								contains.add(entry.getKey());
+							}
+						}
+						for(String str:contains) {
+							App.transactionHashes.get(str).getKey().sendMessage("<@"+App.transactionHashes.get(str).getValue()+">さんの取引を含んだブロックがマイニングされました").queue();
+						}
+					}
+				}catch(Exception e) {e.printStackTrace();}
+			}
+			System.out.println("thread-Receiver終了");
 		} catch (IOException e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
-		InputStreamReader sock_is = new InputStreamReader(sock_in);
-		BufferedReader sock_br = new BufferedReader(sock_is);
-		String s=null;
-		try {
-			while ((s=sock_br.readLine())!=null) {
-				System.out.println(s);
-				if (s.startsWith("balance~")) {
-					String utxo=s.split("~")[1];
-					String[] arg=utxo.split(",");
-					App.utxo.put(arg[0], new BigDecimal(arg[1]));
-					System.out.println(arg[0]+","+arg[1]);
-				}
-			}
-			System.out.println("thread-Receiver終了");
-		}catch(Exception e) {e.printStackTrace();}
 	};
 }
